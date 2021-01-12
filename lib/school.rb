88ee51af4a7bdf5d6972bb0ce8fdf65757b4e73e
Matt <mattfsewell@gmail.com>
2021-01-12 17:54:17 -0500

    commit tree 666e042820b03002bd6b2804845a7df6977dbf9c

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
-# code here!
+require 'pry'
+class School
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    attr_reader :name
+
+    def roster
+        @roster
+    end
+    def add_student(student, grade)
+        if !@roster[grade]
+            @roster[grade] = []
+        end
+        @roster[grade] << student
+    end
+    def grade(grade)
+        @roster[grade]
+    end
+    def sort
+        @roster.each do |grade_name, name_array|
+            # binding.pry
+            if name_array[0][0] > name_array[1][0]
+                lower = name_array[0]
+                name_array[0] = name_array[1]
+                name_array[1] = lower
+            end
+        end
+    end
+
+
+
+end
